f533a9a0aa8cdd37de60b5bfd7a2c957b86653e4
Wiktor-Sikora <sikora.wiktor777@gmail.com>
2025-05-26 16:58:06 +0200

    visualization css fix, variable fix

--- app/components/arrayFrame.jsx
+++ app/components/arrayFrame.jsx
@@ -16,18 +16,18 @@ function randomArray(arrayLen = 50, maxValue = 50, minValue = 0) {
 function Element({ id, value, maxValue, state= 1 }) {
     let bgColor = "bg-white"
     if (state === 0) {
-        let bgColor = "bg-white"
-    } else {
-        let bgColor = "bg-rosepink"
+        bgColor = "bg-white"
+    } else if (state === 2) {
+        bgColor = "bg-rosepink"
     }
 
-    return (<div className={`w-full h-[${Math.floor(value / maxValue * 100)}%] ${ bgColor }`} key={id} ></div>)
+    return (<div className={`w-full h-[${Math.floor(value / maxValue * 100)}%] ${ bgColor }`} style={{height: Math.floor(value / maxValue * 100) + '%'}} key={id} ></div>)
 }
 
 export default function ArrayFrame({ arrayLen = 50, maxValue = 50, minValue = 0 }) {
     const [elements, setElements] = useState(randomArray(arrayLen, maxValue, minValue).sort((a, b) => a.value - b.value));
 
-    return (<div className="flex flex-row gap-1 w-full">
+    return (<div className="flex flex-row items-end gap-1 w-full">
         {elements.map(element => (
             <Element key={element.id} value={element.value} maxValue={maxValue} state={element.state} />
         ))}
